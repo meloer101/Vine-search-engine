b6fc4c5b8e5560dfd2946c610dec301851779333
Jacoy <jacobyu80@gmail.com>
2025-12-10 18:23:41 +0800

    logo完成

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className={darkTheme ? 'dark' : ''}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen ">
-        <NavBar />
+        <NavBar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
         <Routes />
         <Footer />
       </div>
--- src/conponents/NavBar.jsx
+++ src/conponents/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const NavBar = () => {
+export const NavBar = ({darkTheme, setDarkTheme}) => {
   return (
     <div className="p-5 pb-0 flex flex-wrap sm:justfy-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
-      <div className="felx justify-between items-center space-x-5 w-screen  ">
+      <div className="flex justify-between items-center space-x-5 w-screen  ">
         {/* Logo Section */}
         <Link to="/">
           <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900 ">
@@ -13,8 +13,14 @@ export const NavBar = () => {
         </Link>
 
         {/*dark mode toggle button */}
+        <button
+          type="button"
+          onClick={() => setDarkTheme(!darkTheme)}
+        >
+          {darkTheme ? 'Light 💡' : 'Dark 🌙'}
+        </button>
 
-        
+
       </div>
     </div>
   );
