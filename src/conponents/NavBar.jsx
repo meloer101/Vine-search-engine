e70e890cfbf7a7a6e552dff4a52849ed5628f74c
Jacoy <jacobyu80@gmail.com>
2025-12-10 18:05:02 +0800

    logo设置完毕

--- src/conponents/NavBar.jsx
+++ src/conponents/NavBar.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <div className="p-5 pb-0 flex flex-wrap sm:justfy-between justify-center items-center border-b dark:border-gray-700 border-gray-200" >
-      <div className="felx justify-between items-center space-x-5 w-screen  " >
-        <Link to="/" className="text-2xl font-bold text-blue-700 dark:text-blue-300" >
-        <p>
+    <div className="p-5 pb-0 flex flex-wrap sm:justfy-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
+      <div className="felx justify-between items-center space-x-5 w-screen  ">
+        {/* Logo Section */}
+        <Link to="/">
+          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900 ">
             Vine Search 🔍
-        </p>
+          </p>
         </Link>
+
+        {/*dark mode toggle button */}
+
+        
       </div>
     </div>
   );
