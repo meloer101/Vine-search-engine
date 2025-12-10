a300069817be7ccb144ab4065e0fc398a168d395
Jacoy <jacobyu80@gmail.com>
2025-12-10 19:12:32 +0800

    dark mode完成crazy哈哈哈

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
 
   return (
-    <div className={darkTheme ? 'dark' : ''}>
+    <div className={darkTheme ? 'dark' : ''} class="dark">
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen ">
         <NavBar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
         <Routes />
