19071015a7c59e7aca53812369874dbb1e45a219
Jacoy <jacobyu80@gmail.com>
2025-12-10 18:29:49 +0800

    进行dark mode挑事之前的最新版本

--- src/conponents/NavBar.jsx
+++ src/conponents/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const NavBar = ({darkTheme, setDarkTheme}) => {
+export const NavBar = ({ darkTheme, setDarkTheme }) => {
   return (
     <div className="p-5 pb-0 flex flex-wrap sm:justfy-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
       <div className="flex justify-between items-center space-x-5 w-screen  ">
@@ -16,11 +16,10 @@ export const NavBar = ({darkTheme, setDarkTheme}) => {
         <button
           type="button"
           onClick={() => setDarkTheme(!darkTheme)}
+          className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg hover:scale-105 transition-all duration-300"
         >
-          {darkTheme ? 'Light 💡' : 'Dark 🌙'}
+          {darkTheme ? "Light 💡" : "Dark 🌙"}
         </button>
-
-
       </div>
     </div>
   );
