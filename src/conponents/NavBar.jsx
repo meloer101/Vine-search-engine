73e2ba66fb8d6e2257d34c078cb454b8c3ad9b59
Jacoy <jacobyu80@gmail.com>
2025-12-10 18:40:18 +0800

    加入dark mode作用前的最终版本

--- src/conponents/NavBar.jsx
+++ src/conponents/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const NavBar = ({ darkTheme, setDarkTheme }) => {
   return (
-    <div className="p-5 pb-0 flex flex-wrap sm:justfy-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
       <div className="flex justify-between items-center space-x-5 w-screen  ">
         {/* Logo Section */}
         <Link to="/">
