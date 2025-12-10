4937f3ec8df6c19184ec1574c7040b7a67f74935
Jacoy <jacobyu80@gmail.com>
2025-12-10 17:53:42 +0800

    对navbar进行logo，设置好router

--- src/conponents/NavBar.jsx
+++ src/conponents/NavBar.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
     <div className="p-5 pb-0 flex flex-wrap sm:justfy-between justify-center items-center border-b dark:border-gray-700 border-gray-200" >
-      <h1>NavBar</h1>
+      <div className="felx justify-between items-center space-x-5 w-screen  " >
+        <Link to="/" className="text-2xl font-bold text-blue-700 dark:text-blue-300" >
+        <p>
+            Vine Search 🔍
+        </p>
+        </Link>
+      </div>
     </div>
   );
 };
