f0de102c6bd69cd2e5e7e5fe55f26c1b971ff542
Jacoy <jacobyu80@gmail.com>
2025-12-10 16:53:09 +0800

    加入组件以及tailwind，初始化完毕

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { NavBar } from './conponents/NavBar'
+import { Footer } from './conponents/Footer'
+import { Routes } from './conponents/Routes'
 
-function App() {
+
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello</h1>
+      <NavBar />
+      <Routes />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App
