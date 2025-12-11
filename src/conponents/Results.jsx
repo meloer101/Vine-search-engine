c0127689e56b72a8bdd88f88883ed8a8aa0049f5
Jacoy <jacobyu80@gmail.com>
2025-12-11 19:22:18 +0800

    完成组件Loading

--- src/conponents/Results.jsx
+++ src/conponents/Results.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import  { useLoaction } from 'react-router-dom';
+import ReactPlayer from 'react-player';
 
+import {useResultContext} from '../contexts/ResultContextProvider'; 
 
 export const Results = () => {
+  const { results, isLoading, getResults, searchTerm } = useResultContext();
+const location = useLoaction();
+
+if (isLoading) return <Loading/>;
+
   return (
     <div>
         <h1>Results</h1>
